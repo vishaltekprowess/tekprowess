f8d8d768ed48c5c1213ee14f8974beb4168f390d
vishaltekprowess <vishal@tekprowess.com>
2025-07-21 13:23:44 +0530

    [INIT]

---
+++ custom_invoice/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Invoice",
+
+    'summary': "Custom Invoice Layout and Features",
+
+    'description': """
+This module provides a custom invoice layout and additional features for managing invoices in Odoo.
+    """,
+
+    'author': "Tekprowess",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','web','account','l10n_in'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        "report/custom_layout.xml",
+        # "report/report_invoice.xml"
+    ],
+}
+
